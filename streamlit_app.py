ef3dd7732761077f0306642a199bf3386a56c0ea
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-01-23 09:43:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,32 @@
-
 import streamlit as st
 import openai
 
-# Pedir la clave de OpenAI
+# Pedir la clave de OpenAI al usuario
 st.title("Autenticación de OpenAI")
 openai_key = st.text_input("Ingrese su clave de OpenAI")
 
-if openai_key:
-    openai.api_key = openai_key
+# Botón para autenticar
+if st.button("Ingresar"):
+    # Aviso de confirmación
     st.success("Clave de OpenAI confirmada")
+
+# Pedir el texto al usuario
+st.title("Análisis de texto")
+text = st.text_area("Ingrese el texto a analizar")
 
 # Botón para analizar
 if st.button("Analizar"):
-    # Pedir el texto a analizar
-    text = st.text_area("Escriba el texto a analizar")
-
+    # Autenticar con la clave de OpenAI
+    openai.api_key = openai_key
     # Usar GPT-3 para encontrar el problema y la conclusión
-    if text:
-        prompt = f"""
-        Problem: {text}
-        Conclusion:
-        """
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=prompt,
-            max_tokens=150,
-            temperature=0.7,
-            top_p=0.9,
-        )
-        conclusion = response["choices"][0]["text"]
-        st.success(conclusion)
+    completion = openai.Completion()
+    response = completion.create(
+        prompt=text,
+        engine="text-davinci-003",
+        temperature=0.7,
+        max_tokens=150,
+        top_p=1,
+    )
+    # Mostrar el resultado
+    st.write("**Problema:**", response["choices"][0]["text"])
+    st.write("**Conclusión:**", response["choices"][1]["text"])
