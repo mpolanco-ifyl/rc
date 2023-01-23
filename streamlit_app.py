4ba9f6e61991aeba5afc207b6d606d2e8cf17250
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-01-23 09:24:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,40 @@
 import streamlit as st
 import openai
 
-# Solicitando la API Key de OpenAI
-openai_api_key = st.text_input("Por favor introduzca la API Key de OpenAI:")
+# Pedir la API key de OpenAI al usuario
+st.title('Analizador de texto con GPT-3')
+st.write('Ingrese su API key de OpenAI para autenticarse:')
+api_key = st.text_input('API Key')
 
-# Inicializando la API
-openai.api_key = openai_api_key
+# Botón para autenticarse
+if st.button('Ingresar'):
+    openai.api_key = api_key
 
-# Texto del artículo
-article_text = st.text_area(
-    "Introduzca el texto del artículo aquí:")
+# Pedir el texto al usuario
+st.write('Ingrese el texto a analizar:')
+texto = st.text_area('Texto')
 
-# Obteniendo el problema y conclusión
-if article_text:
-    gpt = openai.Completion.create(
+# Botón para analizar
+if st.button('Analizar'):
+    # Usar GPT-3 para encontrar el problema y la conclusión
+    prompt = f"Problem: {texto}"
+    response = openai.Completion.create(
         engine="davinci",
-        prompt=article_text,
-        max_tokens=500,
+        prompt=prompt,
+        max_tokens=100,
         temperature=0.7,
-        top_p=1,
-        n=1,
+        top_p=1
     )
+    problema = response['choices'][0]['text']
+    st.write('Problema:', problema)
 
-    problem_text = gpt['choices'][0]['text']
-    conclusion_text = gpt['choices'][1]['text']
-
-    # Mostrando los resultados
-    st.write("El problema es:")
-    st.write(problem_text)
-    st.write("La conclusión es:")
-    st.write(conclusion_text)
+    prompt = f"Conclusion: {texto}"
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=100,
+        temperature=0.7,
+        top_p=1
+    )
+    conclusion = response['choices'][0]['text']
+    st.write('Conclusión:', conclusion)
