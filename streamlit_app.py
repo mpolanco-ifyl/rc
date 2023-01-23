5298138ee957a04675eb66d27fd7a50e24e73008
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-01-23 09:41:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,32 @@
+
 import streamlit as st
 import openai
 
 # Pedir la clave de OpenAI
-st.title('Analizador de texto con OpenAI GPT-3')
-st.write('Por favor ingrese su clave de OpenAI para continuar.')
-openai_key = st.text_input('Clave de OpenAI')
+st.title("Autenticación de OpenAI")
+openai_key = st.text_input("Ingrese su clave de OpenAI")
 
-# Autenticar la clave de OpenAI
 if openai_key:
     openai.api_key = openai_key
-    st.success('Clave de OpenAI autenticada exitosamente.')
-
-# Pedir el texto a analizar
-if openai_key:
-    st.write('Ingrese el texto que desea analizar.')
-    text = st.text_area('Texto')
-
-# Analizar el texto
-if openai_key and text:
-    prompt = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt="encuentre el problema y la conclusión; ponga el problema en forma de pregunta",
-        max_tokens=150,
-        temperature=0.7,
-        top_p=1
-    )
-    conclusion = prompt.choices[0].text
-    st.write('Pregunta:', prompt.prompt)
-    st.write('Conclusión:', conclusion)
+    st.success("Clave de OpenAI confirmada")
 
 # Botón para analizar
-if openai_key and text:
-    if st.button('Analizar'):
-        st.write('Pregunta:', prompt.prompt)
-        st.write('Conclusión:', conclusion)
+if st.button("Analizar"):
+    # Pedir el texto a analizar
+    text = st.text_area("Escriba el texto a analizar")
+
+    # Usar GPT-3 para encontrar el problema y la conclusión
+    if text:
+        prompt = f"""
+        Problem: {text}
+        Conclusion:
+        """
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=150,
+            temperature=0.7,
+            top_p=0.9,
+        )
+        conclusion = response["choices"][0]["text"]
+        st.success(conclusion)
